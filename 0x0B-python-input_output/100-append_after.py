ac726f27e65dcf313522c1c9751f2b95e3e6b410
ghitabnr <ghita.bnr2003@gmail.com>
2023-10-15 17:30:37 +0100

    ppp

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,14 +1,20 @@
-#!usr/bin/python3
-"""appden after a string"""
+#!/usr/bin/python3
+"""
+Contains the "append after" function
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """appden after a string"""
-    new_str = ""
-    with open(filename, "r") as f:
-        for line in f:
-            new_str += line
+    """appends "new_string" after a line containing
+    "search_string" in "filename" """
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
             if search_string in line:
-                new_str += new_string
-    with open(filename, "w") as f:
-        f.write(new_str)
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)
