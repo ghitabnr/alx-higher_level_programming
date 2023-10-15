d35830ed21a3b3441d82196b391a790ab87def82
ghitabnr <ghita.bnr2003@gmail.com>
2023-10-15 17:08:23 +0100

    eeett

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,25 +1,30 @@
-#!usr/bin/python3
-"""student"""
+#!/usr/bin/python3
+"""Module for Student class"""
 
 
 class Student:
-    """my student"""
+    """Class that defines a student"""
+
     def __init__(self, first_name, last_name, age):
+        """Initializes an instance of the Student class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """to json"""
+        """Method that gets a dictionary representation
+        of a Student instance"""
         if attrs is None:
             return self.__dict__
-        new_dict = {}
-        for key, value in self.__dict__.items():
-            if key in attrs:
-                new_dict[key] = value
-        return new_dict
+        else:
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
 
     def reload_from_json(self, json):
-        """reload from json"""
+        """This function reloads the Student
+        instance from a JSON"""
         for key, value in json.items():
-            self.__dict__[key] = value/
+            self.__dict__[key] = value
