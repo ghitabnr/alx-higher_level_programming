06e6fb32db309ae36c02bebd8f1bbe7c274a6bb5
ghitabnr <ghita.bnr2003@gmail.com>
2023-10-15 17:24:17 +0100

    nnn

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-'''task 7 module'''
-
+"""adds all arguments to a Python list, and then save them to a file"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arglist = list(sys.argv[1;])
 
-try:
-        old_date = load_from_json_file('add_item.json')
-except Exception:
-        old_date = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-old_date.extend(arglist)
-save_to_json_file(old_date, 'add_item.json')
+    try:
+        list = load_from_json_file("add_item.json")
+    except Exception:
+        list = []
+    list.extend(sys.argv[1:])
+    save_to_json_file(list, "add_item.json")
