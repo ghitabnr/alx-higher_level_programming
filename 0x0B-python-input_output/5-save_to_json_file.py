97b34b2ee938f9d2cfd1b8acc58fb2bfba5eb3aa
Ghita Bounouara <133217535+ghitabnr@users.noreply.github.com>
2023-11-10 20:43:17 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/5-save_to_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""save_to_json_file"""
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """save_to_json_file"""
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(json.dumps(my_obj))
