4eb74b76bc1027a68d21922d52d36c7f953edac8
ghitabnr <ghita.bnr2003@gmail.com>
2024-02-15 13:37:32 +0100

    xxxxxx

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function factorial (n) {
+    if (isNaN(n)) {
+      return 1;
+    } else if (n <= 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  const arg = parseInt(process.argv[2]);
+  const result = factorial(arg);
+  
+  console.log(result);
